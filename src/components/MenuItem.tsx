204c4bcb8f520b2ef65f27a35d6e8b7a38ef3315
Pablo Avila <pabloavila@MacBook-Air-de-Pablo.local>
2024-07-31 15:43:37 -0600

    Agregando la clase active:ring al boton de menuItem

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -9,7 +9,9 @@ type MenuItemProps = {
 export default function MenuItem({ item, addItem }: MenuItemProps) {
   return (
     <>
-    <button className="border-2 hover:bg-lime-200 border-lime-100 w-full px-2 py-1 flex justify-between items-center"
+    <button className="border-2 border-lime-100 w-full px-2 py-1 flex justify-between items-center
+    hover:bg-lime-200 active:bg-lime-300 active:ring-2 active:ring-lime-400
+    sm:active:bg-lime-400 sm:active:ring-lime-500"
     onClick={ () => addItem(item) }
     >
      <img className="h-20 w-20" 
